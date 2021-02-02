a6d77e2f62c3a4e552e996fa871e1e418d7cde29
rainyang3 <77300717+rainyang3@users.noreply.github.com>
2021-02-03 01:54:46 +0800

    text

--- java.js
+++ java.js
@@ -90,7 +90,7 @@ class Particle {
 //creating the particles array
 function init() {
     particlesArray = []; //creating array of particle objects, instead of list.
-    let numberOfParticles = (canvas.height * canvas.width) / 6000; // Math for amount of particles in relation to the area of the canvas size.
+    let numberOfParticles = (canvas.height * canvas.width) / 8000; // Math for amount of particles in relation to the area of the canvas size.
     for ( let i=0; i< numberOfParticles; i++) {
         let size = (Math.random() * 5) + 1; // random number from 1-6
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
@@ -156,6 +156,82 @@ window.addEventListener('mouseout',
 
 )
 
+// //cursor
+// var cursor = {
+//   delay: 8,
+//   _x: 0,
+//   _y: 0,
+//   endX: (window.innerWidth / 2),
+//   endY: (window.innerHeight / 2),
+//   cursorVisible: true,
+//   cursorEnlarged: false,
+//   $dot: document.querySelector('.cursor-dot'),
+//   $outline: document.querySelector('.cursor-dot-outline'),
+  
+//   cursor: function() {
+//       // Set up element sizes
+//       this.dotSize = this.$dot.offsetWidth;
+//       this.outlineSize = this.$outline.offsetWidth;
+      
+//       this.setupEventListeners();
+//       this.animateDotOutline();
+//   },
+//   document.addEventListener('mouseenter', function(e) {
+//     self.cursorVisible = true;
+//     self.toggleCursorVisibility();
+//     self.$dot.style.opacity = 1;
+//     self.$outline.style.opacity = 1;
+// });
+
+// document.addEventListener('mouseleave', function(e) {
+//     self.cursorVisible = true;
+//     self.toggleCursorVisibility();
+//     self.$dot.style.opacity = 0;
+//     self.$outline.style.opacity = 0;
+// });
+// },
+
+// animateDotOutline: function() {
+// var self = this;
+
+// self._x += (self.endX - self._x) / self.delay;
+// self._y += (self.endY - self._y) / self.delay;
+// self.$outline.style.top = self._y + 'px';
+// self.$outline.style.left = self._x + 'px';
+
+// requestAnimationFrame(this.animateDotOutline.bind(self));
+// },
+
+// toggleCursorSize: function() {
+// var self = this;
+
+// if (self.cursorEnlarged) {
+//     self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+//     self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+// } else {
+//     self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+//     self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+// }
+// },
+
+// toggleCursorVisibility: function() {
+// var self = this;
+
+// if (self.cursorVisible) {
+//     self.$dot.style.opacity = 1;
+//     self.$outline.style.opacity = 1;
+// } else {
+//     self.$dot.style.opacity = 0;
+//     self.$outline.style.opacity = 0;
+// }
+// }
+// }
+
+// cursor.cursor();
+  
+
 init();
 animate();
+
+
 
